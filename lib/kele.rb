c884c266cf5d4451dca895ceb299befceb84616e
Alex Gould <alexgould17@gmail.com>
2017-04-05 22:58:32 -0700

    Retrieved user info (stored in @user_info, accessed via .get_me).

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,18 @@
 require 'httparty'
 class Kele
   include HTTParty
+  attr_reader :auth_token
+
   def initialize(email, password)
     bloc_response = self.class.post("https://www.bloc.io/api/v1/sessions", { body: {
       email: email,
       password: password
     }})
-    puts "Response:"
-    puts bloc_response
+    @user_info = bloc_response["user"]
+    @auth_token = bloc_response["auth_token"]
+  end
+
+  def get_me
+    @user_info
   end
 end
