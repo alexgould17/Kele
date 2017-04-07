38236f8fe4ec840611ffc8efade9151b6321ceb6
Alex Gould <alexgould17@gmail.com>
2017-04-07 10:53:49 -0700

    Stored parsed data in user_info and made get_mentor_availability return an array of starting times as timecode strings.

--- lib/kele.rb
+++ lib/kele.rb
@@ -12,17 +12,22 @@ class Kele
   end
 
   def get_me
+    @user_info = {}
     raw_user_info = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { authorization: @auth_token })
+    raw_user_info.parsed_response.each do |key, value|
+      @user_info[key] = value
+    end
+    @user_info
   end
 
   def get_mentor_availability(mentor_id)
-    self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability",
-      headers: {
-        authorization: @auth_token
-      },
-      body: {
-        id: mentor_id
-      }
-    )
+    raw_mentor_data = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability",
+      headers: { authorization: @auth_token },
+      body: { id: mentor_id })
+    available_times = []
+    raw_mentor_data.parsed_response.each do |slot|
+      available_times << slot["starts_at"] unless slot["booked"]
+    end
+    available_times
   end
 end
