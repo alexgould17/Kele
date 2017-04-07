496f0071bd6ccc1785eb74024818415ad09c6402
Alex Gould <alexgould17@gmail.com>
2017-04-07 10:32:34 -0700

    All the data comes in correctly now, will parse into a more readable format in the next commit/before I turn in the assignment.

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,18 +1,28 @@
 require 'httparty'
 class Kele
   include HTTParty
-  attr_reader :auth_token
+  attr_reader :auth_token, :user_info
 
   def initialize(email, password)
     bloc_response = self.class.post("https://www.bloc.io/api/v1/sessions", { body: {
       email: email,
       password: password
     }})
-    @user_info = bloc_response["user"]
     @auth_token = bloc_response["auth_token"]
   end
 
   def get_me
-    @user_info
+    raw_user_info = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { authorization: @auth_token })
+  end
+
+  def get_mentor_availability(mentor_id)
+    self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability",
+      headers: {
+        authorization: @auth_token
+      },
+      body: {
+        id: mentor_id
+      }
+    )
   end
 end
