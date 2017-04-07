b1ecd7c992c379c3819727b5afdefe98006d6ce0
Alex Gould <alexgould17@gmail.com>
2017-04-07 15:05:41 -0700

    Added .gitignore and adding a test helper module locally (with a few minor changes).

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -10,4 +10,18 @@ module Roadmap
     headers: { authorization: @auth_token })
     response.parsed_response
   end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    raise "Must have user info populated before sending a message!" unless @user_info && @user_info["current_enrollment"]["id"]
+    response = self.class.post("https://www.bloc.io/api/v1/messages", {
+      authorization: @auth_token,
+      values: {
+        assignment_branch: assignment_branch,
+        assignment_commit_link: assignment_commit_link,
+        checkpoint_id: checkpoint_id,
+        comment: comment,
+        enrollment_id: @user_info["current_enrollment"]["id"]
+      }})
+    response.parsed_response
+  end
 end
